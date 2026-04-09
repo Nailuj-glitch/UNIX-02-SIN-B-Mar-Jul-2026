7f518ddfc22a353fbdba1ef2be939c3cc7e82fe5
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-04-09 13:25:16 +0000

    New folder creation

---
+++ script_boot.sh
@@ -0,0 +1,45 @@
+#  makes a directory called proyecto
+mkdir proyecto 
+
+#  changes directory to proyecto as a relative root
+cd proyecto/ 
+
+#  returns to the absolute root
+cd .. 
+
+#  Changes the directory to /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto as an absolute root
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto 
+
+
+# ruta absoluta: empieza con /
+
+
+# Ways to enter to home directory
+
+# Changes directory 
+cd
+
+# Changes directory to home
+cd ~
+
+# Using an absolute root, the directory is changed to home
+cd /home/codespace
+
+# Shows the home directory of the current user
+echo $HOME
+
+# Displays the bash play executable thats being used
+echo $BASH
+
+#S hows the PATH variable, which is a list of directories where Linux looks for executable programs when you type a comman
+echo $PATH
+
+
+
+
+
+
+
+
+
+ 
