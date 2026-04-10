900f8237338e46133a927a6a467c26fdd36b2585
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-04-10 16:58:58 +0000

    Paso 3: Compilar el kernel Linux

--- script_distro.sh
+++ script_distro.sh
@@ -13,3 +13,32 @@ libelf-dev , libssl-dev — dependencias del kernel
 syslinux — el bootloader
 dosfstools — para crear el filesystem FAT
 qemu-system-x86 — para probar la imagen sin necesidad de hardware real
+
+#Compilar el kernel Linux
+#Clona el repositorio del kernel
+git clone --depth 1 https://github.com/torvalds/linux.git
+cd linux
+#Configura las opciones de compilación:
+make menuconfig
+
+#Compila el kernel:
+make -j 2
+
+#Al finalizar, se ve un mensaje indicando que 
+bzImage is ready 
+
+# Copia la imagen del kernel a un directorio de trabajo:
+sudo mkdir /boot-files
+sudo cp arch/x86/boot/bzImage /boot-files/
+cd ..
+
+
+#Paso 4: Compilar BusyBox
+
+git clone --depth 1 https://git.busybox.net/busybox
+cd busybox
+
+#Configura BusyBox:
+make menuconfig
+
+
