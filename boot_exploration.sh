61100c265c0ff14016db1be579a5313b91cc3552
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-04-16 13:06:34 +0000

    help options

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -10,7 +10,26 @@ ls -l -a -h
 ls -lah
 
 # to create a directory which has - , you input --
-mkdir -- -rf # -- means that the text after the symbol is just text
+mkdir -- -rf # -- means that the text after the symbol is taken as arguments
 
 rmdir -- -rf # removes the directory
+
+# Help otpions 
+ls --help
+man ls
+
+# to search
+first / to be able to type in
+# then type in what you want to search 
+n # to go forwrd 
+N # to go backwards
+q # to quit
+
+
+man git #git manual
+
+--depth 
+# Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless
+# --no-single-branch is given to fetch the histories near the tips of all branches. If you want to clone submodules shallowly,
+# also pass --shallow-submodules.
 
