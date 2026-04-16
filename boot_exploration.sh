1f8a0a19a07451aea375467808057da3c6156e94
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-04-16 12:46:52 +0000

    ls commands

---
+++ boot_exploration.sh
@@ -0,0 +1,11 @@
+ls -a == ls --all #they do exactly the same the long one is clearer on a script, while a short one is faster to type in
+
+# this options are the same 
+ls -l # shows detailed information about each file
+ls -a # shows all files
+ls -h # displays file sizes in an easier-to-read format
+
+# they list all files
+ls -l -a -h
+ls -lah
+
