55076cbd6d0bbd0421729c6fcc06ae8fb3dde8d3
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-05-28 13:21:09 +0000

    creating directory in desarrolladores

--- security2.sh
+++ security2.sh
@@ -34,3 +34,37 @@ ls -la ~/antes_de_newgrp.txt
 # Verify group change
 id -gn
  desarrolladores
+
+# Create new file inside the subshell
+
+touch ~/dentro_de_newgrp.txt
+
+ls -la ~/dentro_de_newgrp.txt
+ -rw-r--r-- 1 root desarrolladores 0 may 28 13:13 /root/dentro_de_newgrp.txt
+
+# The group is 'desarrolladores' now
+
+# Create a directory
+mkdir -p ~/proyecto_dev/src
+
+ls -la ~/
+    total 64
+    drwx------ 1 root root             4096 may 28 13:15 .
+    drwxr-xr-x 1 root root             4096 may 28 12:16 ..
+    -rw-r--r-- 1 root root                0 may 28 12:53 antes_de_newgrp.txt
+    -rw-r--r-- 1 root root             5578 may 24 04:01 .bashrc
+    -rw-r--r-- 1 root root              607 may 24 04:01 .bashrc.original
+    drwxr-xr-x 3 root root             4096 may 28 12:16 .cache
+    drwx------ 3 root root             4096 may 28 12:16 .config
+    -rw-r--r-- 1 root desarrolladores     0 may 28 13:13 dentro_de_newgrp.txt
+    lrwxrwxrwx 1 root root               47 may 28 12:16 .docker -> /workspaces/.codespaces/.persistedshare/.docker
+    drwxr-xr-x 3 root root             4096 may 28 12:16 .dotnet
+    drwx------ 4 root root             4096 may 28 12:17 .local
+    -rw-r--r-- 1 root root              132 may 15 11:37 .profile
+    drwxr-xr-x 3 root desarrolladores  4096 may 28 13:15 proyecto_dev
+    drwx------ 2 root root             4096 may 28 12:16 .ssh
+    -rw-r--r-- 1 root root                0 may 28 12:45 test_grupo_heredado.txt
+    drwxr-xr-x 5 root root             4096 may 28 12:16 .vscode-remote
+    -rw-r--r-- 1 root root            11180 may 28 12:16 .zshrc
+
+
