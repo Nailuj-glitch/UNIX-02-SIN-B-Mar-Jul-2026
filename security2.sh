81c94cb917ccc10986c3805f7c9c373c6a48e6e7
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-05-28 12:56:11 +0000

    viewing & creating file before newgrp

--- security2.sh
+++ security2.sh
@@ -13,4 +13,15 @@ ls -la ~/test_grupo_heredado.txt
 
 # The group is the main group of the main user
 
+#Viewing main group
+id -gn
 
+#echo "Grupo actual: $(id -gn);}
+
+# Create a file before newgrp
+touch ~/antes_de_newgrp.txt
+
+ls -la ~/antes_de_newgrp.txt
+ -rw-r--r-- 1 root root 0 may 28 12:53 /root/antes_de_newgrp.txt
+
+ 
