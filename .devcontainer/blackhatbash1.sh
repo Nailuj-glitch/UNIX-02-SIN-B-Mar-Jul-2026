661955e8029a1a7fdf7230f3fd30c13be0a1d89c
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-06-04 12:33:48 +0000

    bash version intro

---
+++ .devcontainer/blackhatbash1.sh
@@ -0,0 +1,22 @@
+bash --version
+    #GNU bash, version 5.3.3(1)-release (x86_64-alpine-linux-musl)
+    #Copyright (C) 2025 Free Software Foundation, Inc.
+    #License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+
+env # Variable de entorno
+
+
+echo ${SHELL}
+    #/bin/bash
+
+
+echo ${RANDOM}
+    # 22282
+
+
+echo ${UID}
+    #1000
+
+
+echo ${OSTYPE}
+    # linux-musl
