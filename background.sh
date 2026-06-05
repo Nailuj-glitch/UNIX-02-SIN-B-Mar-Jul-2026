e3e012a46348d445be304ffd5895f1c4e08a4b6e
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-06-05 16:49:09 +0000

    Streams

---
+++ background.sh
@@ -0,0 +1,11 @@
+echo "Sleeping for 30 seconds..." # manda a dormir al proceso
+sleep 30 # & # lo manda al background
+
+# Creates a file
+echo "Creating the file test123"
+
+touch test123
+
+echo "deleting the file test123"
+rm test123
+
---
+++ blackhatbash5.sh
@@ -0,0 +1,7 @@
+top # muestra en tiempo real los procesos del sistema y el uso de recursos como CPU y memoria.
+
+top& #ejecuta top en segundo plano, permitiendo seguir usando la terminal mientras el comando continúa corriendo
+
+kill -9 (process ID) # envía la señal SIGKILL a un proceso para finalizarlo de forma inmediata y forzada.
+kill -9 8120 # Ejemplo
+
