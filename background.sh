7fec5a1575786d6418e93cc4c8808e94e8ab3e44
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-06-05 17:15:47 +0000

    redirection operators

--- background.sh
+++ background.sh
@@ -5,6 +5,7 @@ sleep 30 # & # lo manda al background
 echo "Creating the file test123"
 
 touch test123
+sleep 30 # & # lo manda al background
 
 echo "deleting the file test123"
 rm test123
--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -5,3 +5,36 @@ top& #ejecuta top en segundo plano, permitiendo seguir usando la terminal mientr
 kill -9 (process ID) # envía la señal SIGKILL a un proceso para finalizarlo de forma inmediata y forzada.
 kill -9 8120 # Ejemplo
 
+
+touch test && touch test123 #crea test y, si tiene éxito, crea también test123.
+
+(ls; ps) ejecuta # ls y ps dentro de un subshell (subproceso). 
+
+ls; ps; whoami  # ejecuta secuencialmente ls, luego ps y finalmente whoami.
+
+lzl || echo "el comando lzl fallo" # intenta ejecutar lzl; si falla, muestra el mensaje "el comando lzl fallo".
+
+
+
+echo "Hello World!" > output.txt
+cat output.txt
+
+
+echo "Chao mundo" > output.txt # se sobreescribe
+cat output.txt
+
+echo "Chao Chao" >> output.txt # no se sobreescribe por >>
+
+
+ls -l / &> stdout_and_stderr.txt
+cat stdout_and_stderr.txt
+
+ls -l / &>> stdout_and_stderr.txt # se duplica ls -l
+
+
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt
+
+lzl 2> error.txt
+cat stderr.txt
