6b48f42f5a95c500544251557b68a81de5ab902a
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-06-12 17:04:16 +0000

    for_ip

---
+++ for_ip.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for ip_address in "$@"; do
+
+    echo "Taking some action on IP address ${ip_address}"
+
+done
+
+# Taking some action on IP address 192.168.1.10
+# Taking some action on IP address 192.168.1.20
+# Taking some action on IP address 10.0.0.5
