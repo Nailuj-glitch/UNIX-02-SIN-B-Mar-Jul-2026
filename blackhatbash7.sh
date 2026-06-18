1f9013431a3480865ac35b3204422483be2dda3b
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-06-18 13:10:09 +0000

    changing the default limiter

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -21,3 +21,13 @@ awk '{print $1,$2,$3}' log.txt
 awk '{print $2}' log.txt
 
 awk '{print $1,$NF}' log.txt
+
+awk -F',' '{print $1}' example.txt
+
+awk 'NR < 10' log.txt
+
+grep "42.236.10.117" log.txt
+
+awk '{print $7}' log.txt
+
+grep "42.236.10.117" log.txt | awk '{print $7}'
