a8cb38084c4b21e64b47be38ffd6d8aa5016268e
Nailuj-glitch <jvillalbarubio@gmail.com>
2026-06-18 12:42:26 +0000

    ps use

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -3,3 +3,7 @@
 grep "35.237.4.214" log.txt
 
 grep "35.237.4.214\|13.66.139.0" log.txt
+
+ps | grep TTY # Filtra los procesos mostrados por ps y muestra solo las líneas que contienen TTY en mayúsculas.
+
+ps | grep -i tty # Filtra los procesos mostrados por ps y muestra las líneas que contienen tty sin distinguir entre mayúsculas y minúsculas.
